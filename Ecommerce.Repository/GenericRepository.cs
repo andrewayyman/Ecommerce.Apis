6baba768c1aacdc7d172011d26994943c1920a3e
andrew ayman <andrewayman1000@gmail.com>
2024-10-22 02:27:53 +0300

    GenericRepo

---
+++ Ecommerce.Repository/GenericRepository.cs
@@ -0,0 +1,36 @@
+﻿using Ecommerce.Core.Entites;
+using Ecommerce.Core.Repository.Contract;
+using Ecommerce.Repository.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+    {
+        private readonly StoreContext _dbContext;
+
+        public GenericRepository(StoreContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
+        public async Task<T> GetAsync( int id )
+        {
+            return await _dbContext.Set<T>().FindAsync(id);
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbContext.Set<T>().ToListAsync();
+
+        }
+
+     
+    }
+}
